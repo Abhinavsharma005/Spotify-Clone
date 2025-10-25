217c7b9e4f2c43fba33f246bbd91700c80115f39
Abhinavsharma005 <sharmaabhinav1013@gmail.com>
2025-10-25 20:34:52 +0530

    back to original code

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,9 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   base: '/Spotify-Clone/',
-//   plugins: [react()],
-// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Define the base path for GitHub Pages
-const REPO_NAME = 'Spotify-Clone'; // Replace with your exact repository name
-const BASE_PATH = process.env.NODE_ENV === 'production' ? `/${REPO_NAME}/` : '/';
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  // 💡 CRITICAL FIX: Set the base path
-  base: BASE_PATH, 
-  
+  base: '/Spotify-Clone/',
   plugins: [react()],
 })
+
